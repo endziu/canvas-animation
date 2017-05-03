5316f1b9a1be53132f785879f48d32072cc7815b
endziu <koper.andrzej@gmail.com>
2017-05-03 12:50:06 +0200

    prepare deploy

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express()
 
 app.use(express.static('public'))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(16662, function () {
+  console.log('Example app listening on port 16662!')
 })
