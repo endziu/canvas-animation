aa17ff08dfae5fc0d874dfd6e5c0a95433a6b78b
endziu <koper.andrzej@gmail.com>
2017-09-03 15:56:36 +0200

    logs

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
+var morgan = require('morgan')
 
 app.use(express.static('public'))
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.listen(16663, function() {
   console.log('Example app listening on port 16663!')
 })
